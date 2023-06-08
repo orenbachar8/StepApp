daf57551d812fbee363eff0f3964133390683214
OrenB <orenbachar03@gmail.com>
2023-06-08 23:45:17 +0300

    Styling the map

--- screens/MyRoute.js
+++ screens/MyRoute.js
@@ -5,41 +5,30 @@ import MapViewDirections from 'react-native-maps-directions';
 
 const userIcon = require('./user-icon.png');
 const destinationIcon = require('./destination-icon.png');
-const discountIcon = require('./discount-icon.png');
+const discountIcon1 = require('./Tamara.png');
+const discountIcon2 = require('./Sumsum.png');
+const discountIcon3 = require('./carrefour.png');
 const selectedDiscountIcon = require('./selected-discount-icon.png');
 
+const userLocation = { latitude: 32.081702, longitude: 34.773038 };
+const destinationLocation = { latitude: 32.093891, longitude: 34.776715 };
+const discountLocations = [
+  { id: 1, name: 'Tamara Smoothies', coordinate: { latitude: 32.084712, longitude: 34.774348 } },
+  { id: 2, name: 'Sumsum Salads', coordinate: { latitude: 32.085946, longitude: 34.774644 } },
+  { id: 3, name: 'Carrefour', coordinate: { latitude: 32.091100, longitude: 34.776011 } },
+];
+
 export default function MyRoute({ navigation }) {
-  const [userLocation, setUserLocation] = useState(null);
-  const [destinationLocation, setDestinationLocation] = useState(null);
-  const [discountLocations, setDiscountLocations] = useState([]);
   const [selectedDiscounts, setSelectedDiscounts] = useState([]);
 
-  useEffect(() => {
-    // Set the user's location to Tel Aviv, Israel
-    const userLocation = { latitude: 32.0853, longitude: 34.7818 };
-
-    // Set the destination location to a different coordinate
-    const destinationLocation = { latitude: 32.0858, longitude: 34.7811 };
-
-    // Set the discount business locations
-    const discountLocations = [
-      { id: 1, name: 'Coffee Shop 1', coordinate: { latitude: 32.0851, longitude: 34.7814 } },
-      { id: 2, name: 'Smoothie Bar', coordinate: { latitude: 32.0853, longitude: 34.7809 } },
-      { id: 3, name: 'Restaurant', coordinate: { latitude: 32.0855, longitude: 34.7806 } },
-    ];
-
-    setUserLocation(userLocation);
-    setDestinationLocation(destinationLocation);
-    setDiscountLocations(discountLocations);
-  }, []);
-
   const toggleDiscountSelection = (discountId) => {
-    const isSelected = selectedDiscounts.includes(discountId);
-    if (isSelected) {
-      setSelectedDiscounts(selectedDiscounts.filter(id => id !== discountId));
-    } else {
-      setSelectedDiscounts([...selectedDiscounts, discountId]);
-    }
+    setSelectedDiscounts((prevSelectedDiscounts) => {
+      if (prevSelectedDiscounts.includes(discountId)) {
+        return prevSelectedDiscounts.filter((id) => id !== discountId);
+      } else {
+        return [...prevSelectedDiscounts, discountId];
+      }
+    });
   };
 
   const getInitialRegion = () => {
@@ -60,73 +49,84 @@ export default function MyRoute({ navigation }) {
     return null;
   };
 
-  const initialRegion = getInitialRegion();
+  const getDiscountLogoStyle = (discountId) => {
+    return selectedDiscounts.includes(discountId)
+      ? styles.selectedDiscountLogo
+      : styles.discountLogo;
+  };
+
+  const getDiscountTextStyle = (discountId) => {
+    return selectedDiscounts.includes(discountId)
+      ? styles.selectedDiscountText
+      : styles.discountText;
+  };
+
+  const getDiscountCoordinates = () => {
+    if (selectedDiscounts.length > 0) {
+      return selectedDiscounts.map((discountId) => {
+        const discountLocation = discountLocations.find((location) => location.id === discountId);
+        return discountLocation.coordinate;
+      });
+    } else {
+      return [];
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} initialRegion={initialRegion}>
+      <MapView style={styles.map} initialRegion={getInitialRegion()}>
         {/* Render the user location marker */}
-        {userLocation && (
-          <Marker coordinate={userLocation}>
-            <Image source={userIcon} style={styles.markerIcon} />
-          </Marker>
-        )}
+        <Marker coordinate={userLocation}>
+          <Image source={userIcon} style={styles.markerIcon} />
+        </Marker>
+
         {/* Render the destination location marker */}
-        {destinationLocation && (
-          <Marker coordinate={destinationLocation}>
-            <Image source={destinationIcon} style={styles.markerIcon} />
-          </Marker>
-        )}
+        <Marker coordinate={destinationLocation}>
+          <Image source={destinationIcon} style={styles.markerIcon} />
+        </Marker>
+
         {/* Render the path between userLocation, selected discount locations, and destinationLocation */}
-        {userLocation && destinationLocation && (
-          <>
-            <MapViewDirections
-              origin={userLocation}
-              destination={destinationLocation}
-              waypoints={selectedDiscounts.map(discountId => {
-                const discountLocation = discountLocations.find(location => location.id === discountId);
-                return discountLocation.coordinate;
-              })}
-              strokeWidth={3}
-              strokeColor="blue"
-            />
-            <Polyline
-              coordinates={[
-                userLocation,
-                ...selectedDiscounts.map(discountId => {
-                  const discountLocation = discountLocations.find(location => location.id === discountId);
-                  return discountLocation.coordinate;
-                }),
-                destinationLocation,
-              ]}
-              strokeWidth={3}
-              strokeColor="green"
-            />
-          </>
-        )}
+        <Polyline
+          coordinates={[
+            userLocation,
+            ...getDiscountCoordinates(),
+            destinationLocation,
+          ]}
+          strokeWidth={3}
+          strokeColor="green"
+        />
+
         {/* Render the discount locations markers */}
-        {discountLocations.map(discountLocation => (
+        {discountLocations.map((discountLocation) => (
           <Marker
             key={discountLocation.id}
             coordinate={discountLocation.coordinate}
             onPress={() => toggleDiscountSelection(discountLocation.id)}
           >
-            <Image source={selectedDiscounts.includes(discountLocation.id) ? selectedDiscountIcon : discountIcon} style={styles.markerIcon} />
+            <View style={styles.discountMarkerContainer}>
+              <Image source={getDiscountIcon(discountLocation.id)} style={styles.discountIcon} />
+              <View
+                style={[
+                  styles.discountIndicator,
+                  selectedDiscounts.includes(discountLocation.id) && styles.selectedDiscountIndicator,
+                ]}
+              />
+            </View>
           </Marker>
         ))}
       </MapView>
+
       <View style={styles.discountSection}>
         {/* Render the discounts section */}
         <Text style={styles.discountTitle}>Discounts on the Way:</Text>
-        {discountLocations.map(discountLocation => (
-          <Text
-            key={discountLocation.id}
-            style={selectedDiscounts.includes(discountLocation.id) ? styles.selectedDiscountText : styles.discountText}
-          >
-            {discountLocation.name}
-          </Text>
+        {discountLocations.map((discountLocation) => (
+          <View key={discountLocation.id} style={styles.discountItemContainer}>
+            <Image source={getDiscountIcon(discountLocation.id)} style={getDiscountLogoStyle(discountLocation.id)} />
+            <Text style={getDiscountTextStyle(discountLocation.id)}>{discountLocation.name}</Text>
+          </View>
         ))}
       </View>
+
       <View style={styles.buttonContainer}>
         <Button title="HomeScreen" onPress={() => navigation.navigate('HomeScreen')} />
         <Button title="MyRoute" onPress={() => navigation.navigate('MyRoute')} />
@@ -161,13 +161,32 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 8,
   },
+  discountItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  discountLogo: {
+    width: 32,
+    height: 32,
+    marginRight: 8,
+    borderWidth: 2,
+    borderColor: 'red',
+    backgroundColor: 'pink',
+  },
+  selectedDiscountLogo: {
+    width: 32,
+    height: 32,
+    marginRight: 8,
+    borderWidth: 2,
+    borderColor: 'green',
+    backgroundColor: 'pink',
+  },
   discountText: {
     fontSize: 16,
-    marginBottom: 4,
   },
   selectedDiscountText: {
     fontSize: 16,
-    marginBottom: 4,
     fontWeight: 'bold',
   },
   buttonContainer: {
@@ -176,5 +195,34 @@ const styles = StyleSheet.create({
     width: '100%',
     marginTop: 16,
   },
+  discountMarkerContainer: {
+    alignItems: 'center',
+  },
+  discountIcon: {
+    width: 32,
+    height: 32,
+  },
+  discountIndicator: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: 'red',
+    marginTop: 2,
+  },
+  selectedDiscountIndicator: {
+    backgroundColor: 'green',
+  },
 });
 
+function getDiscountIcon(discountId) {
+  switch (discountId) {
+    case 1:
+      return discountIcon1;
+    case 2:
+      return discountIcon2;
+    case 3:
+      return discountIcon3;
+    default:
+      return discountIcon1;
+  }
+}
